cd94e4112cb832b2fd77f563de82d14f29ac939d
Hrithik Reddy <158307841+HrithikD1@users.noreply.github.com>
2024-07-14 00:23:17 +0000

    Venv

--- my-app/src/Pages/Timetable.jsx
+++ my-app/src/Pages/Timetable.jsx
@@ -3,12 +3,35 @@ import axios from "axios";
 
 export default function Timetable(){
     const [data, setData] = useState();
+
     useEffect(() => {
         axios.get("/backend")
         .then(response => setData(response.data))
+        .then(console.log(data))
         .catch(error => console.error(error))
-    })
+    }, [])
+
     return(
-        <></>
-    )
+        <div className="App">
+        <h1>Timetable</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Subject</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.subject}</td>
+                <td>{item.startTime}</td>
+                <td>{item.endTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
 }
